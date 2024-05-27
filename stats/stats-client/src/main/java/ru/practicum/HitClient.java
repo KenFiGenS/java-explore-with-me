a47e8eb7ecc0449abc18eaebf39f8e80ab924954
Kenga <sng.inc@yandex.ru>
2024-05-27 16:46:27 +0500

    feat: настроил HitClient и StatsClient

--- stats/stats-client/src/main/java/ru/practicum/HitClient.java
+++ stats/stats-client/src/main/java/ru/practicum/HitClient.java
@@ -1,6 +1,5 @@
 package ru.practicum;
 
-
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -14,17 +13,15 @@ import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.statsDto.StatsDtoCreate;
-import ru.practicum.statsDto.StatsDtoWithHitsCount;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
-public class StatsClient {
-    private static final String API_PREFIX = "/stats";
+public class HitClient {
+    private static final String API_PREFIX = "/hit";
     private RestTemplate restTemplate;
 
-    public StatsClient(@Value("${stats-server.url}") String serverUrl, RestTemplateBuilder builder) {
+    public HitClient(@Value("${stats-server.url}") String serverUrl, RestTemplateBuilder builder) {
         this.restTemplate = builder
                 .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
                 .requestFactory(HttpComponentsClientHttpRequestFactory::new)
@@ -32,23 +29,13 @@ public class StatsClient {
     }
 
     @SneakyThrows
-    public List<StatsDtoWithHitsCount> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
-        String url;
-        if (uris.isEmpty() && unique == null) {
-            url = "?start=" + start + "&end=" + end;
-        } else if (!uris.isEmpty() && unique == null) {
-            url = "?start=" + start + "&end=" + end + "&uris=" + uris;
-        } else if (!uris.isEmpty()) {
-            url = "?start=" + start + "&end=" + end + "&unique=" + unique;
-        } else {
-            url = "?start=" + start + "&end=" + end + "&uris=" + uris + "&unique=" + unique;
-        }
+    public StatsDtoCreate createHit(StatsDtoCreate statsDtoCreate) {
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(List.of(MediaType.APPLICATION_JSON));
         headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<StatsDtoCreate> entityReq = new HttpEntity<>(headers);
+        HttpEntity<StatsDtoCreate> entityReq = new HttpEntity<>(statsDtoCreate, headers);
         try {
-            return restTemplate.exchange(url, HttpMethod.GET, entityReq, List.class).getBody();
+            return restTemplate.exchange("", HttpMethod.POST, entityReq, StatsDtoCreate.class).getBody();
         } catch (HttpStatusCodeException e) {
             throw new IllegalArgumentException(e.getMessage());
         }
