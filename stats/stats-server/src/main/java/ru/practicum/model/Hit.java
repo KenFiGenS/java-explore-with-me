f904d4e928adea691937120541069eba368ca284
Kenga <sng.inc@yandex.ru>
2024-05-28 20:19:16 +0500

    fix: Правки в схеме

--- stats/stats-server/src/main/java/ru/practicum/model/Hit.java
+++ stats/stats-server/src/main/java/ru/practicum/model/Hit.java
@@ -15,6 +15,7 @@ import java.time.LocalDateTime;
 public class Hit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "HIT_ID")
     private int id;
     private String app;
     private String uri;
